5d8da05705571b577f9995c0c03f853d9beb339d
shel <shel-ne@mail.ru>
2018-05-28 14:57:15 +0300

    -a

--- commit_pu.sh
+++ commit_pu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add ./
 git commit -m $1 -a
-#git remote add origin https://github.com/ShelPablo/EPAM
-git remote set-url origin https://github.com/ShelPablo/EPAM
+git remote add origin https://github.com/ShelPablo/EPAM
+#git remote set-url origin https://github.com/ShelPablo/EPAM
 git push origin master
